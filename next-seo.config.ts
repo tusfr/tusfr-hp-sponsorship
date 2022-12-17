679df4f7824c88277a5e248e25ce814d69c6c6d2
Jagashira <pyprogjas.3104@gmail.com>
2022-12-17 16:46:08 +0900

    fix(next-seo.config): seo変更

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,31 +1,31 @@
 import { NextSeoProps } from 'next-seo';
 
 const SEO: NextSeoProps = {
-	defaultTitle: 'Twin:te スポンサー',
-	titleTemplate: `%s | Twin:te スポンサー`,
-	description: 'Twin:te 協賛ページ',
+	defaultTitle: '理科大機械工学研究会 スポンサー',
+	titleTemplate: `%s | 理科大機械工学研究会 スポンサー`,
+	description: '理科大機械工学研究会 協賛ページ',
 	openGraph: {
 		type: 'website',
-		title: 'Twin:te スポンサー',
-		description: 'Twin:te 協賛ページ',
-		url: 'https://sponsorship.twinte.net/',
-		site_name: 'Twin:te Sponsorship',
+		title: '理科大機械工学研究会 スポンサー',
+		description: '理科大機械工学研究会 協賛ページ',
+		url: 'https://tusfr-hp-sponsorship-j.vercel.app/',
+		site_name: 'Tusfr Sponsorship',
 		images: [
 			{
-				url: 'https://sponsorship.twinte.net/images/sponsor-ogp.jpg',
-				alt: 'Twin:te 協賛ページ'
+				url: 'https://tusfr-hp-sponsorship-j.vercel.app/images/tusfrLogo.jpg',
+				alt: '理科大機械工学研究会 協賛ページ'
 			}
 		]
 	},
 	twitter: {
-		handle: '@te_twin',
-		site: '@te_twin',
+		handle: '@tusfr',
+		site: '@tusfr',
 		cardType: 'summary_large_image'
 	},
 	additionalLinkTags: [
 		{
 			rel: 'icon',
-			href: '/favicon.ico'
+			href: '/tusfrLogo.ico'
 		}
 	]
 };
